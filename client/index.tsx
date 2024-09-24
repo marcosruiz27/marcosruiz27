f96f8727be39d95e36153038c865531ebf67b353
Marcos Ruiz <marcosruiz27@gmail.com>
2024-09-24 17:18:28 +1200

    Set up Routes

--- client/index.tsx
+++ client/index.tsx
@@ -2,6 +2,10 @@ import { createRoot } from 'react-dom/client'
 
 import App from './components/App.tsx'
 
+import router from '../client/router.tsx'
+
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(<App />)
+  createRoot(document.getElementById('app') as HTMLElement).render(
+    <RouterProvider router={router} />,
+  )
 })
